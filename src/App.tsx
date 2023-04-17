092113c4aba9454868b9cfd417138bf99ba3dad6
Max Green <maxdgreen@live.com.au>
2023-04-17 23:27:25 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className="flex justify-between items-center px-4 py-3 bg-gray-800 w-full">
+        <h1 className="text-white text-lg font-bold">Max Green's Portfolio</h1>
+        <div className="flex space-x-4">
+          <a href="#" className="text-gray-300 hover:text-white">Button 1</a>
+          <a href="#" className="text-gray-300 hover:text-white">Button 2</a>
+          <a href="#" className="text-gray-300 hover:text-white">Button 3</a>
+          <a href="#" className="text-gray-300 hover:text-white">Button 4</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
