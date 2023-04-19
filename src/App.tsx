d91e8cc5c2de81f61eca31a1f80ff6b16532cda4
Max Green <maxdgreen@live.com.au>
2023-04-19 20:45:09 +1000

    Correctly organise the abstraction of elements for DOM.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import './App.css'
+import Banner from "./Banner"
+import Hero from "./Hero"
 
 function App() {
   
   return (
-    <div className="App">
-      <div className="flex justify-between items-center px-4 py-3 bg-gray-800 w-full">
-        <h1 className="text-white text-lg font-bold">Max Green's Portfolio</h1>
-        <div className="flex space-x-4">
-          <a href="#" className="text-gray-300 hover:text-white">Button 1</a>
-          <a href="#" className="text-gray-300 hover:text-white">Button 2</a>
-          <a href="#" className="text-gray-300 hover:text-white">Button 3</a>
-          <a href="#" className="text-gray-300 hover:text-white">Button 4</a>
-        </div>
-      </div>
+    <div>
+      <Banner />
+      <Hero />
     </div>
   )
 }
