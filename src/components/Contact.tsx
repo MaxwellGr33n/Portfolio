4742d5cef69b0ec88613d7257da5e9a42b48af95
Max Green <maxdgreen@live.com.au>
2023-08-06 20:52:04 +0200

    Altered code for consistent design language and working links to email and linkedin

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,25 +1,30 @@
 export default function Contact() {
     return (
-        <section className="bg-white dark:bg-gray-900">
-            <div className="py-16 lg:py-8 px-4 mx-auto max-w-screen-md">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
-                <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Prefer the old school way? Contact me via maxdgreen.dev@outlook.com</p>
-                <form action="#" className="space-y-8">
-                    <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-neutral-500 focus:border-neutral-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-neutral-500 dark:focus:border-neutral-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
-                    </div>
-                    <div>
-                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                        <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-neutral-500 focus:border-neutral-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-neutral-500 dark:focus:border-neutral-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
-                    </div>
-                    <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                        <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-neutral-500 focus:border-neutral-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-neutral-500 dark:focus:border-neutral-500" placeholder="Leave a comment..."></textarea>
-                    </div>
-                    <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-black rounded-lg bg-neutral-100 sm:w-fit hover:bg-neutral-300 focus:ring-4 focus:outline-none focus:ring-neutral-300 dark:bg-neutral-600 dark:hover:bg-neutral-700 dark:focus:ring-neutral-800">Send message</button>
-                </form>
+        <section className="bg-neutral-50 dark:bg-gray-900 h-32">
+            <div className="flex justify-center">
+                <h1 className="text-3xl dark:text-neutral-50">Contact</h1>
             </div>
+            <div className="flex justify-center bg-neutral-50 dark:bg-gray-900">
+            <div className='grid gap-3 grid-cols-2 md:gap-4 max-w-sm lg:max-w-xl py-3'>
+                <a href="mailto:maxdgreen.dev@outlook.com" target="_blank" rel="noopener noreferrer">
+                    <div className='bg-neutral-50 dark:bg-gray-800 aspect-square rounded-lg shadow-md hover:shadow-xl overflow-hidden'>
+                        <div className="flex justify-center px-3 pt-2">
+                            <img src="src/assets/mail.svg" alt="Email" height="90%" width="90%" />
+                        </div>
+                        <p className="text-center text-sm sm:text-md dark:text-neutral-50">Email</p>
+                    </div>
+                </a>
+                <a href="https://www.linkedin.com/in/maxwell-green-dev/" target="_blank" rel="noopener noreferrer">
+                    <div className='bg-neutral-50 dark:bg-gray-800 aspect-square rounded-lg shadow-md hover:shadow-xl overflow-hidden'>
+                        <div className="flex justify-center px-3 pt-2">
+                            <img src="src/assets/linkedin.svg" alt="LinkedIn" height="90%" width="90%" />
+                        </div>
+                        <p className="text-center text-sm sm:text-md dark:text-neutral-50">LinkedIn</p>
+                    </div>
+                </a>
+            </div>
+            </div>
+             
         </section>
     );
 }
