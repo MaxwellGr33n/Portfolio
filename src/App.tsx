9fd2f36a7df6e85822976c9bcfb35ae02bd4fd0e
Max Green <maxdgreen@live.com.au>
2023-08-06 22:58:30 +0200

    Linting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Banner from "./components/Banner"
-import Hero from "./components/Hero"
-import TechStack from "./components/TechStack"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
+import Banner from './components/Banner'
+import Hero from './components/Hero'
+import TechStack from './components/TechStack'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
-  
   return (
     <>
       <Banner />
