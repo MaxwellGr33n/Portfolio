7a099f13ea19492e5ccdc228b36f6ba04b276860
Max Green <maxdgreen@live.com.au>
2023-08-06 21:38:08 +0200

    Eslint + Prettier linting changes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-import { defaults } from "autoprefixer";
+import { defaults } from 'autoprefixer'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       screens: {
-        xs: "420px",
-      },
-    },
+        xs: '420px'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
